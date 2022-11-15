754fc229292aff63c845877ebe41dc08e27b5ec3
Charles <charles.jouenne@gmail.com>
2022-11-15 18:35:45 +0100

    Certaines routes sont renommées

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use('api/sauces', saucesRoutes);
-app.use('api/auth', userRoutes);
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
