7bb758c616084bdf590b694d79d586b7d517c6f8
Charles <charles.jouenne@gmail.com>
2022-11-16 15:12:46 +0100

    Mise en place de la route createLikeSauce

--- controllers/likes.js
+++ controllers/likes.js
@@ -2,5 +2,7 @@ const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
 exports.createLikeSauce = (req, res, next) => {
-    
+    Sauce.findOne({ _id: req.params.id})
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => { res.status(400).json( { error })})
 }
